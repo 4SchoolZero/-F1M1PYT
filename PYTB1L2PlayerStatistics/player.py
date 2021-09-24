9d3234a59d7867aa668ede5a637e36fce478f910
Tijmen <34179@ma-web.nl>
2021-09-24 10:07:42 +0200

    characterCreation commit

---
+++ PYTB1L2PlayerStatistics/player.py
@@ -0,0 +1,23 @@
+print("Game: GtaRp_RageMP\n")
+name = "Haze Hutton"
+clan = "Fibonacci"
+rank = "Caporegime"
+groupSize = 5
+killCount = 0
+cash = 244
+bankAccount = 460000,50
+age = "17"
+favWeapon = "Revolver"
+vehicle = "LCC Innovation_Customized"
+bounty = 0
+
+print("Naam: " + name)
+print("Leeftijd: " + str(age))
+print("Motor: " + vehicle)
+print("Clan: " + clan)
+print("Rank: " + rank)
+print("Fav_Wapen: " + favWeapon)
+print("Bounty: " + str(bounty))
+print("Volgers: " + str(groupSize))
+print("Portomone " + str(cash))
+print("Bank " + str(bankAccount))
--- PYTB1L2PoemPrinting/poemwriting.py
+++ PYTB1L2PoemPrinting/poemwriting.py
@@ -1,7 +1,8 @@
 print("dit is een gedicht \n")
-print("Hallo, ik ben tijmen en ik kan heel goed rijmen.")
-print("Heel goed is ook een groot wordt, ik moet niet overdrijven")
-print("Voor de opdracht moest ik een gedicht schrijven")
-print("Over je intresses hobbys en meer")
-print("dus ik maak zinnen hierover, keer op keer")
-print("klaar met deze rijm, ik ben tijmen en ik ben niet skeer :)")
+vers1 = " Hallo, ik ben tijmen en ik kan heel goed rijmen.\n"
+vers2 = "Heel goed is ook een groot wordt, ik moet niet overdrijven\n"
+vers3 = "Voor de opdracht moest ik een gedicht schrijven\n"
+vers4 = "Over je intresses hobbys en meer\n"
+vers5 = "dus ik maak zinnen hierover, keer op keer\n"
+vers6 = "klaar met deze rijm, ik ben tijmen en ik ben niet skeer :)\n"
+print(vers1, vers2, vers3, vers4, vers5, vers6)
