88ad7a2e27f84cd65d9f7cf6c8ba4e3840b9632e
Tijmen <34179@ma-web.nl>
2021-10-01 10:08:30 +0200

    operatorsOpdr Done

---
+++ PYTB1L2MyFirstOperations/operators.py
@@ -0,0 +1,17 @@
+toekennen = 1
+optellen = 1+2
+aftrekken = 1-2
+keer =  5*5
+delen =  52/5
+restwaarde = 56%5
+delen += keer 
+toekennen /= aftrekken
+print("alle sommen kunt u zien in de code \n")
+print("toekennen (assignment) = " + str(toekennen))
+print("optellen (addition) + " + str(optellen))
+print("aftrekken (subtraction) - " + str(aftrekken))
+print("keer (multiply) * " + str(keer))
+print("delen (divide) / " + str(delen))
+print("restwaarde (modulo) % " + str(restwaarde))
+print("optellen bij waarde van variabele +=, zie code")
+print("delen door waarde van variabele /=, zie code")
